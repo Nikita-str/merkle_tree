16b9f97db1a90e85dbcbda8802761b3fd966acbc
Nikita-str <newmynamespace@gmail.com>
2025-04-11 14:46:28 +0300

    move auxilarity fns into `utility` mod

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,12 @@
 mod merkle_tree;
 mod hasher;
 
+pub mod utility;
+
 #[cfg(test)]
 mod tests;
 
-pub use merkle_tree::MerkleTree;
+pub use merkle_tree::{MerkleTree, MtLvl};
 pub use hasher::{MtHasher, MtArityHasher, MtDataHasher};
 
 #[cfg(any(feature = "unsecure", test))]
